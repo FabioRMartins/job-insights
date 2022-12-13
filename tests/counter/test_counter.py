0994d76e02346161166ff96790ba2aac260c57d1
FabioMartins <fabbio1000@gmail.com>
2022-12-13 17:40:48 -0300

    Feat: Implementando um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word_python = "Python"
+    word_javascript = "Javascript"
+
+    counter_python = count_ocurrences(path, word_python)
+
+    assert counter_python == 1639
+
+    counter_javascript = count_ocurrences(path, word_javascript)
+
+    assert counter_javascript == 122
